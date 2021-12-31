628746674fee38ad511cfce5f41b1b22b062180a
Sascha Maassen <sascha.maassen@uni-due.de>
2021-12-31 13:49:04 +0100

    add output test

---
+++ tests/test_output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ace_layer.hpp>
+
+int main(int, char**) 
+{
+    // test of the ouput variants of an element
+
+    // Step 1: Load the element file
+    AceGenElement T1_Laplace(std::string(SMSElmt)+"/T1_Laplace.c");
+
+    
+    return 0;
+}
